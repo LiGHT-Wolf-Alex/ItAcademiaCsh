777df281d72a478be7689bba91dc963dfc600593
LiGHT_WolF <74818873+LiGHT-Wolf-Alex@users.noreply.github.com>
2023-10-29 10:08:10 +0300

    Revert "Revert "Task""
    This reverts commit d5809e911ec589847a35764b593fe2774edbd4dc.

---
+++ Homework/30-10Homework5/ExerciseOne.cs
@@ -0,0 +1,12 @@
+﻿namespace ItAcademiaCsh.Homework._30_10Homework5;
+
+public class ExerciseOne : IHomework
+{
+    public string DateIssueTask { get; } = "30.10.2023";
+    public char TaskNumber { get; } = '1';
+
+    public void CompletingTask()
+    {
+        
+    }
+}
---
+++ Homework/30-10Homework5/ExerciseTwo.cs
@@ -0,0 +1,12 @@
+﻿namespace ItAcademiaCsh.Homework._30_10Homework5;
+
+public class ExerciseTwo : IHomework
+{
+    public string DateIssueTask { get; } = "30.10.2023";
+    public char TaskNumber { get; } = '2';
+
+    public void CompletingTask()
+    {
+       
+    }
+}
