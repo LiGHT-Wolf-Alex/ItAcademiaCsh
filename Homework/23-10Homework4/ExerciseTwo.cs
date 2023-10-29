f561568e8706a515c9f5a2cadf519f355095e6e9
aleks <aleks.valmus2001@gmail.com>
2023-10-29 09:34:08 +0300

    Добавлена реализация: Задача №2. Вычислить разность максимума и минимума в массиве и вывести её в консоль. Ввод размера массива и самих значений выполняется в консоли. Методами расширения LINQ не пользоваться!

--- Homework/23-10Homework4/ExerciseTwo.cs
+++ Homework/23-10Homework4/ExerciseTwo.cs
@@ -7,5 +7,33 @@ public class ExerciseTwo : IHomework
 
     public void CompletingTask()
     {
+        Console.Write("Enter the number of array elements => ");
+        int.TryParse(Console.ReadLine(), out var arraySIze);
+
+        var myArray = new float [arraySIze];
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.Write($"[{i}] = ");
+            float.TryParse(Console.ReadLine(), out myArray[i]);
+        }
+
+        float max = myArray[0], min = myArray[0];
+
+        for (int i = 1; i < myArray.Length; i++)
+        {
+            if (max < myArray[i])
+            {
+                max = myArray[i];
+                continue;
+            }
+
+            if (min > myArray[i])
+            {
+                min = myArray[i];
+            }
+        }
+
+        Console.WriteLine($"max = {max}; min = {min}");
+        Console.WriteLine($"Difference between maximum and minimum in an array => {max - min}");
     }
 }
