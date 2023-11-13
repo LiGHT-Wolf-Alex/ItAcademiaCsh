c7edfa77e138f7846ae9bea1e765c181c334c50a
aleks <aleks.valmus2001@gmail.com>
2023-11-13 21:44:18 +0300

    test

---
+++ ControlWork/13-11/IntExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace ItAcademiaCsh.ControlWork._13_11;
+
+public static class IntExtensions
+{
+    public static string ToWords(this int number)
+    {
+
+        var byfer = number;
+        while (byfer != 0)
+        {
+            
+            
+        }
+        string[] units =
+        {
+            "ноль", "один", "два", "три", "четыре",
+            "пять", "шесть", "семь", "восемь", "девять"
+        };
+        if (number == 0 )
+        {
+            return "минус " + Math.Abs(number).ToWords();
+        }
+        return units[number];
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,13 +6,8 @@ class Program
 {
     private static void Main()
     {
-        MyStack stack = new MyStack(5);
-
-        stack.Push(10);
-        stack.Push(20);
-        stack.Push(30);
-
-        Console.WriteLine(stack.Pop());
-        Console.WriteLine(stack.Pop());
+        int number = 1234;
+        string result = number.ToWords();
+        Console.WriteLine(result);
     }
 }
