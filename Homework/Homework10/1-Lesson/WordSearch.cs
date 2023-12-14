84c216dca10059dce6e6916cab4f15e0eafd61d3
aleks <aleks.valmus2001@gmail.com>
2023-12-14 16:55:10 +0300

    Добавил реализацию дз 10 1-2

---
+++ Homework/Homework10/1-Lesson/WordSearch.cs
@@ -0,0 +1,57 @@
+﻿namespace ItAcademiaCsh.Homework.Homework10._1_Lesson;
+
+public class WordSearch
+{
+    private const string InputFilePath = @"input.txt";
+    private const string OutputFilePath = @"output.txt";
+
+    public WordSearch()
+    {
+        try
+        {
+            using (var reader = new StreamReader(InputFilePath))
+            using (var writer = new StreamWriter(OutputFilePath))
+            {
+                while (reader.ReadLine() is { } line)
+                {
+                    var wordCounts = CountWords(line);
+
+                    if (wordCounts.Count > 0)
+                    {
+                        var (key, value) = wordCounts.MaxBy(x => x.Value);
+
+                        writer.WriteLine($"{key} {value}");
+                    }
+                }
+            }
+
+            Console.WriteLine("Done! The results have been written to the output file.");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found. Please make sure the input file exists and try again.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+        }
+    }
+
+    private Dictionary<string, int> CountWords(string? text)
+    {
+        var wordCounts = new Dictionary<string, int>();
+
+        var words = text?.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (words == null) return wordCounts;
+        foreach (var word in words)
+        {
+            if (!wordCounts.TryAdd(word, 1))
+            {
+                wordCounts[word]++;
+            }
+        }
+
+        return wordCounts;
+    }
+}
---
+++ Homework/Homework10/2-lesson/ForbiddenWords.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ItAcademiaCsh.Homework.Homework10._2_lesson;
+
+public class ForbiddenWords
+{
+    private const string ForbiddenWordsFilePath = "forbidden_words.txt";
+
+    public ForbiddenWords()
+    {
+        Console.Write("Введите путь к файлу с текстом: ");
+        var textFilePath = Console.ReadLine();
+
+        var text = ReadFile(textFilePath);
+        if (text == null)
+        {
+            Console.WriteLine("Ошибка при чтении файла с текстом.");
+            return;
+        }
+
+        var forbiddenWords = ReadForbiddenWords(ForbiddenWordsFilePath);
+        if (forbiddenWords == null)
+        {
+            Console.WriteLine("Ошибка при чтении файла с запрещенными словами.");
+            return;
+        }
+
+        var censoredText = CensorText(text, forbiddenWords);
+        Console.WriteLine(censoredText);
+    }
+
+    private string? ReadFile(string filePath)
+    {
+        try
+        {
+            return File.ReadAllText(filePath);
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    private string[]? ReadForbiddenWords(string filePath)
+    {
+        try
+        {
+            return File.ReadAllLines(filePath)
+                .SelectMany(line => line.Split(' '))
+                .Select(word => word.ToLower())
+                .ToArray();
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    private string CensorText(string text, string[] forbiddenWords)
+    {
+        var pattern = string.Join("|", forbiddenWords.Select(word => "\\b" + word + "\\b"));
+        return Regex.Replace(text, pattern, match => new string('*', match.Length), RegexOptions.IgnoreCase);
+    }
+}
