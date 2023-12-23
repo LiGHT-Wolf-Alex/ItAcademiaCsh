3980a84098f72371d8e2da14777243cb4b0881b6
aleks <aleks.valmus2001@gmail.com>
2023-12-23 16:14:26 +0300

    Add homework 11

---
+++ Homework11/LogProcessor.cs
@@ -0,0 +1,50 @@
+﻿namespace Test;
+
+public class LogProcessor
+{
+    public event EventHandler? ErrorOccurred;
+
+    public List<LogEntry> FilterByLevel(List<LogEntry> logs, LogLevel level)
+    {
+        return logs.Where(log => log.Level == level).ToList();
+    }
+
+    public int CountErrors(List<LogEntry> logs)
+    {
+        return logs.Count(log => log.Level == LogLevel.Error);
+    }
+
+    public List<LogEntry> FindRecentLogs(List<LogEntry> logs, DateTime since)
+    {
+        return logs.Where(log => log.Timestamp > since).ToList();
+    }
+
+    public Dictionary<LogLevel, int> GroupByLevel(List<LogEntry> logs)
+    {
+        return logs
+            .GroupBy(log => log.Level)
+            .ToDictionary(group => group.Key, group => group.Count());
+    }
+
+    public List<LogEntry> FindTopLogs(List<LogEntry> logs, string keyword, int count)
+    {
+        return logs
+            .Where(log => log.Message.Contains(keyword, StringComparison.OrdinalIgnoreCase))
+            .OrderByDescending(log => log.Timestamp)
+            .Take(count)
+            .ToList();
+    }
+
+    public void CheckErrors(List<LogEntry> logs)
+    {
+        if (logs.Any(log => log.Level == LogLevel.Error))
+        {
+            OnErrorOccurred();
+        }
+    }
+
+    protected virtual void OnErrorOccurred()
+    {
+        ErrorOccurred?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ Homework11/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Test;
+
+public static class Program
+{
+    static void Main()
+    {
+        List<LogEntry> logs = GenerateLogs();
+        LogProcessor logProcessor = new LogProcessor();
+        logProcessor.ErrorOccurred += HandleErrorOccurred;
+
+        var errorLogs = logProcessor.FilterByLevel(logs, LogLevel.Error);
+        Console.WriteLine($"Количество ошибок: {logProcessor.CountErrors(logs)}");
+
+        var recentLogs = logProcessor.FindRecentLogs(logs, DateTime.Now.AddHours(-1));
+        foreach (var log in recentLogs)
+        {
+            Console.WriteLine($"Recent log: {log.Timestamp} - {log.Level} - {log.Message}");
+        }
+
+        var groupedLogs = logProcessor.GroupByLevel(logs);
+        foreach (var group in groupedLogs)
+        {
+            Console.WriteLine($"{group.Key} count: {group.Value}");
+        }
+
+        var topLogs = logProcessor.FindTopLogs(logs, "ключевое слово", 5);
+        foreach (var log in topLogs)
+        {
+            Console.WriteLine($"Top log: {log.Timestamp} - {log.Level} - {log.Message}");
+        }
+
+        logProcessor.CheckErrors(logs);
+    }
+
+    static List<LogEntry> GenerateLogs()
+    {
+        return
+        [
+            new LogEntry { Timestamp = DateTime.Now, Level = LogLevel.Info, Message = "Информационное сообщение 1" },
+            new LogEntry
+                { Timestamp = DateTime.Now, Level = LogLevel.Warning, Message = "Предупреждающее сообщение 1" },
+            new LogEntry { Timestamp = DateTime.Now, Level = LogLevel.Error, Message = "Сообщение об ошибкe 1" }
+        ];
+    }
+
+    static void HandleErrorOccurred(object sender, EventArgs e)
+    {
+        Console.WriteLine("Возникла ошибка!");
+    }
+}
